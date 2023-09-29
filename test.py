486ce775365bcddb452efb050f4ac2e9735d967b
node12123124 <138673538+node12123124@users.noreply.github.com>
2023-09-29 18:44:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ openai.api_base = "https://zukijourney.xyzbot.net/v1"
 def get_chatgpt_response(input_text):
     chat_completion = openai.ChatCompletion.create(
         stream=False,
-        model="3.5-turbo",
+        model="llama2",
         messages=[
             {
                 "role": "user",
